c9777308a266aadcb2d01ce8ee4c65f4d978e2df
Rémi Calixte <remi.calixte@student.ecp.fr>
2017-12-19 13:26:44 +0100

    add auth middleware

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const fs = require('fs');
+const jwt = require('express-jwt');
+
+const publicKey = fs.readFileSync('./src/oauth.pem');
 
 // const initializeDummyData = require('./tests/dataBaseCreationTest');
 // const printData = require('./tests/printDataTest');
@@ -14,6 +18,16 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
+
+app.use(jwt({
+  secret: publicKey,
+  getToken: (req) => {
+    if (req.get('token')) {
+      return req.get('token');
+    }
+    return null;
+  },
+}).unless({ path: ['/auth'] }));
 
 app.use('/auth', authRouter);
 
