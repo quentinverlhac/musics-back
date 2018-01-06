7500d8626743f12a39092a705c4b83cd9c0a6961
Hodys <quentin.verlhac@student.ecp.fr>
2018-01-06 16:32:29 +0100

    refactor: changed FindOne by FindById in rooms.controller

--- src/controllers/rooms.controller.js
+++ src/controllers/rooms.controller.js
@@ -15,8 +15,7 @@ async function getAllRooms(req, res, next) {
 // Get the information about the room which id given in the url
 async function getRoom(req, res, next) {
   try {
-    const room = await models.room.findOne({
-      where: { roomId: req.params.roomId },
+    const room = await models.room.findById(req.params.roomId, {
       include: [models.instrument],
     });
     res.send(room);
@@ -28,7 +27,7 @@ async function getRoom(req, res, next) {
 // Update the information of the room which id is given in the url
 async function updateRoom(req, res, next) {
   try {
-    const room = await models.room.findOne({ where: { roomId: req.params.roomId } });
+    const room = await models.room.findById(req.params.roomId);
     room.capacity = req.body.capacity;
     room.photoPath = req.body.photoPath;
     room.purpose = req.body.purpose;
@@ -43,11 +42,10 @@ async function updateRoom(req, res, next) {
 // Add the given instrument to the room which id is given in the url
 async function addRoomInstrument(req, res, next) {
   try {
-    const room = await models.room.findOne({ where: { roomId: req.params.roomId } });
+    const room = await models.room.findById(req.params.roomId);
     const instrument = await models.instrument.findById(req.body.instrumentId);
     await room.addInstrument(instrument, { through: { } });
-    const roomInstruments = await models.room.findOne({
-      where: { roomId: req.params.roomId },
+    const roomInstruments = await models.room.findById(req.params.roomId, {
       include: [models.instrument],
     });
     res.send(roomInstruments);
@@ -59,14 +57,12 @@ async function addRoomInstrument(req, res, next) {
 // Delete the given instrument from the room which id is given in the url
 async function deleteRoomInstrument(req, res, next) {
   try {
-    const room = await models.room.findOne({
-      where: { roomId: req.params.roomId },
+    const room = await models.room.findById(req.params.roomId, {
       include: [models.instrument],
     });
     const instrument = await models.instrument.findById(req.body.instrumentId);
     await room.removeInstrument(instrument, { through: { } });
-    const roomInstruments = await models.room.findOne({
-      where: { roomId: req.params.roomId },
+    const roomInstruments = await models.room.findById(req.params.roomId, {
       include: [models.instrument],
     });
     res.send(roomInstruments);
