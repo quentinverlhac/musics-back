e101a64514afed41623b5fb71539f16a526bfb2b
Hodys <quentin.verlhac@student.ecp.fr>
2018-01-08 14:10:53 +0100

    add route to getCurrentUserReservations

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -25,6 +25,70 @@ async function getCurrentUser(req, res, next) {
   }
 }
 
+
+// Update the phone of the connected user
+async function updateCurrentUserPhone(req, res, next) {
+  try {
+    const user = await models.user.findOne({ where: { login: req.user.user.login } });
+    user.telephone = req.body.telephone;
+    await user.save();
+    res.send(user);
+  } catch (e) {
+    next(e);
+  }
+}
+
+
+// Add the given instrument to the logged user
+async function addCurrentUserInstrument(req, res, next) {
+  try {
+    const user = await models.user.findOne({
+      where: { login: req.user.user.login },
+    });
+    const instrument = await models.instrument.findById(req.body.instrumentId);
+    await user.addInstrument(instrument, { through: { } });
+    const userInstruments = await models.user.findOne({
+      where: { login: req.user.user.login },
+      include: [models.instrument],
+    });
+    res.send(userInstruments);
+  } catch (e) {
+    next(e);
+  }
+}
+
+// Add the given instrument to the logged user
+async function deleteCurrentUserInstrument(req, res, next) {
+  try {
+    const user = await models.user.findOne({
+      where: { login: req.user.user.login },
+      include: [models.instrument],
+    });
+    const instrument = await models.instrument.findById(req.body.instrumentId);
+    await user.removeInstrument(instrument, { through: { } });
+    const userInstruments = await models.user.findOne({
+      where: { login: req.user.user.login },
+      include: [models.instrument],
+    });
+    res.send(userInstruments);
+  } catch (e) {
+    next(e);
+  }
+}
+
+// Get the reservations of the logged user
+async function getCurrentUserReservations(req, res, next) {
+  try {
+    const user = await models.user.findOne({
+      where: { login: req.user.user.login },
+    });
+    const reservations = await user.getReservations();
+    res.send(reservations);
+  } catch (e) {
+    next(e);
+  }
+}
+
 // Get the information about the user whose login is given in the url
 async function getUser(req, res, next) {
   try {
@@ -51,55 +115,6 @@ async function updateUserRights(req, res, next) {
   }
 }
 
-// Update the phone of the connected user
-async function updateCurrentUserPhone(req, res, next) {
-  try {
-    const user = await models.user.findOne({ where: { login: req.user.user.login } });
-    user.telephone = req.body.telephone;
-    await user.save();
-    res.send(user);
-  } catch (e) {
-    next(e);
-  }
-}
-
-// Add the given instrument to the logged user
-async function addUserInstrument(req, res, next) {
-  try {
-    const user = await models.user.findOne({
-      where: { login: req.user.user.login },
-    });
-    const instrument = await models.instrument.findById(req.body.instrumentId);
-    await user.addInstrument(instrument, { through: { } });
-    const userInstruments = await models.user.findOne({
-      where: { login: req.user.user.login },
-      include: [models.instrument],
-    });
-    res.send(userInstruments);
-  } catch (e) {
-    next(e);
-  }
-}
-
-// Add the given instrument to the logged user
-async function deleteUserInstrument(req, res, next) {
-  try {
-    const user = await models.user.findOne({
-      where: { login: req.user.user.login },
-      include: [models.instrument],
-    });
-    const instrument = await models.instrument.findById(req.body.instrumentId);
-    await user.removeInstrument(instrument, { through: { } });
-    const userInstruments = await models.user.findOne({
-      where: { login: req.user.user.login },
-      include: [models.instrument],
-    });
-    res.send(userInstruments);
-  } catch (e) {
-    next(e);
-  }
-}
-
 // Get a user's reservations
 async function getUserReservations(req, res, next) {
   try {
@@ -117,11 +132,12 @@ async function getUserReservations(req, res, next) {
 module.exports = {
   getAllUsers,
   getCurrentUser,
+  updateCurrentUserPhone,
+  addCurrentUserInstrument,
+  deleteCurrentUserInstrument,
+  getCurrentUserReservations,
   getUser,
   updateUserRights,
-  updateCurrentUserPhone,
-  addUserInstrument,
-  deleteUserInstrument,
   getUserReservations,
 };
 
