e981a0cc7d1fe18f53b35a6b61dc81e7109246e8
Hodys <quentin.verlhac@student.ecp.fr>
2018-01-21 19:03:08 +0100

    add adherent validation for creating/deleting reservations

--- src/validations/reservations.validation.js
+++ src/validations/reservations.validation.js
@@ -16,17 +16,21 @@ const reservation = Joi.object().keys({
 });
 
 const checkDate = async (req, res, next) => {
-  if (Date.parse(req.body.beginning) > Date.parse(req.body.end)) {
-    res.status(400).send('Error: the beginning of the reservation starts after the end');
-  } else {
-    const availableRooms = await searchAvailableRooms.search(req.body.beginning, req.body.end);
-    const availableIds = availableRooms.map(entity => entity.dataValues.id);
-    const id = req.body.roomId;
-    if (availableIds.includes(id)) {
-      next();
+  try {
+    if (Date.parse(req.body.beginning) > Date.parse(req.body.end)) {
+      res.status(400).send('Error: the beginning of the reservation starts after the end');
     } else {
-      res.status(400).send('Error: the room is not available');
+      const availableRooms = await searchAvailableRooms.search(req.body.beginning, req.body.end);
+      const availableIds = availableRooms.map(entity => entity.dataValues.id);
+      const id = req.body.roomId;
+      if (availableIds.includes(id)) {
+        next();
+      } else {
+        res.status(400).send('Error: the room is not available');
+      }
     }
+  } catch (e) {
+    next(e);
   }
 };
 
@@ -35,21 +39,25 @@ async function checkRightsForReservation(req, res, next) {
     const currentUser = await models.user.findOne({
       where: { login: req.user.user.login },
     });
-    let id = req.body.userId;
-    if (req.params.reservationId) {
-      const reservationToCheck = await models.reservation.findById(req.params.reservationId);
-      if (reservationToCheck) {
-        id = reservationToCheck.userId;
-      } else {
-        res.status(404).send('Reservation not found');
+    if (currentUser.get('adherent')) {
+      let id = req.body.userId;
+      if (req.params.reservationId) {
+        const reservationToCheck = await models.reservation.findById(req.params.reservationId);
+        if (reservationToCheck) {
+          id = reservationToCheck.userId;
+        } else {
+          res.status(404).send('Reservation not found');
+        }
       }
-    }
-    if (currentUser.get('admin')) {
-      next();
-    } else if (currentUser.get('id') === id) {
-      next();
+      if (currentUser.get('admin')) {
+        next();
+      } else if (currentUser.get('id') === id) {
+        next();
+      } else {
+        res.status(403).send('Unauthorized request: The login sent is not the login of connected user (you), and you don\'t have admin rights');
+      }
     } else {
-      res.status(403).send('Unauthorized request: The login sent is not the login of connected user (you), and you don\'t have admin rights');
+      res.status(403).send('Unauthorized request: the connect user is not adherent to MUSICS');
     }
   } catch (e) {
     next(e);
