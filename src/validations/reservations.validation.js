4aba95af508825603596e0e0253723fe47eef06c
Hodys <quentin.verlhac@student.ecp.fr>
2018-01-21 01:53:09 +0100

    add room availability to reservation validation

--- src/validations/reservations.validation.js
+++ src/validations/reservations.validation.js
@@ -1,4 +1,5 @@
 const Joi = require('joi');
+const searchAvailableRooms = require('../misc/search.available.rooms');
 
 const date = Joi.object().keys({
   beginning: Joi.date().required(),
@@ -12,11 +13,17 @@ const reservation = Joi.object().keys({
   end: Joi.date().required(),
 });
 
-const checkDate = (req, res, next) => {
+const checkDate = async (req, res, next) => {
   if (Date.parse(req.body.beginning) > Date.parse(req.body.end)) {
     res.status(400).send('Error: the beginning of the reservation starts after the end');
   } else {
-    next();
+    const availableRooms = await searchAvailableRooms.search(req.body.beginning, req.body.end);
+    const availableIds = availableRooms.map(entity => entity.dataValues.id);
+    if (availableIds.includes(req.body.roomId)) {
+      next();
+    } else {
+      res.status(400).send('Error: the room is not available');
+    }
   }
 };
 
